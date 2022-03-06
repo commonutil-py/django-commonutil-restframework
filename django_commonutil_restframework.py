4043f44799a23588dd83a75ec35c885efdd88aa2
Cheyin L <cheyinl@gmail.com>
2022-03-06 19:27:56 +0800

    drop unnecessary feature flag

--- django_commonutil_restframework.py
+++ django_commonutil_restframework.py
@@ -3,8 +3,6 @@
 Utilities for Django REST Framework
 """
 
-from __future__ import unicode_literals
-
 
 def _make_targeted_get_serializer(serializer_cls, instance_cls):
 	def get_serializer(self, instance=None, *args, **kwds):  # pylint: disable=unused-argument
@@ -23,7 +21,6 @@ def attach_targeted_get_serializer(serializer_cls, instance_cls):
 		serializer_cls - Class object for serializer
 		instance_cls - Class object for data instance
 	"""
-
 	def f(cls):
 		cls.get_serializer = _make_targeted_get_serializer(serializer_cls, instance_cls)
 		return cls
